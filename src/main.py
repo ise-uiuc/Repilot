d12ded2bca1de6ae4d5da86ee3a12a5557fe8aa4
Yuxiang Wei <yuxiang630.wei@gmail.com>
2022-09-29 00:59:24 -0700

    Change iterations

--- src/main.py
+++ src/main.py
@@ -258,7 +258,7 @@ for bug_id, bug in dataset.all_bugs().items():
     # if bug_id != 'Mockito-1':
     #     continue
     print(bug_id)
-    patch_groups = repair_proj(result_dir, bug_id, bug, 2)
+    patch_groups = repair_proj(result_dir, bug_id, bug, 300)
     # candidate_patch_groups: List[int] = []
     # for idx, patch_group in enumerate(patch_groups):
     #     if validate_proj(bug_id, bug, patch_group):
