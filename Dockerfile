cd5131c38b3d1b3aeadd355f86c4c5f6bdeb0d48
Yuxiang Wei <yuxiang630.wei@gmail.com>
2023-08-24 16:25:15 -0500

    feat: create Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,63 @@
+FROM ubuntu:22.04
+
+# Install dependencies
+RUN apt -y update \
+    && apt -y install software-properties-common \
+    && add-apt-repository -y ppa:deadsnakes/ppa
+RUN apt install -y \
+    python3.10 \
+    python3-pip \
+    git \
+    python-is-python3 \
+    subversion \
+    curl \
+    cpanminus \
+    unzip
+
+# Set the working directory to /root
+
+# Install coursier
+WORKDIR /root
+
+RUN mkdir bin \
+    && cd bin \
+    && curl -fL "https://github.com/coursier/launchers/raw/master/cs-x86_64-pc-linux.gz" | gzip -d > cs \
+    && chmod +x cs \
+    && yes n | ./cs setup
+
+ENV PATH="/root/bin:${PATH}"
+
+RUN cs java --jvm 8 --env \
+    && cs java --jvm 11 --env \
+    && cs java --jvm 18 --env
+
+# Install the modified version of the Eclipse JDT Language Server
+RUN git clone https://github.com/UniverseFly/eclipse.jdt.ls --depth 1 \
+    && cd eclipse.jdt.ls \
+    && eval $(cs java --jvm 11 --env) \
+    && ./mvnw clean verify -DskipTests
+
+# Copy the current directory contents into the container
+COPY . Repilot
+RUN cd Repilot \
+    && pip install --upgrade pip \
+    && pip install -e . \
+    && git config --global user.email "repilot@example.com" \
+    && git config --global user.name "Repilot" \
+    && git config --global init.defaultBranch main \
+    && git init \
+    # Repilot requires at least one commit message as of now
+    && git add README.md \
+    && git commit -m "Welcome to Repilot!"
+
+# Install Defects4J
+RUN git clone https://github.com/rjust/defects4j \
+    && cd defects4j \
+    && git checkout v2.0.0 \
+    && cpanm --installdeps . \
+    && ./init.sh
+
+# Generate the meta_config.json
+WORKDIR /root/Repilot
+RUN python generate_meta_config_in_docker.py \
+    && python -m repilot.cli.init
---
+++ generate_meta_config_in_docker.py
@@ -0,0 +1,70 @@
+from pathlib import Path
+import json
+import subprocess
+import re
+
+
+def get_cmd_output(cmd: list[str]) -> str:
+    return subprocess.run(cmd, text=True, capture_output=True).stdout
+
+
+def detect_javahome(output: str) -> str:
+    # extract ... from export JAVA_HOME="..."
+    # the number of whitespaces is not fixed
+    pattern = re.compile(r'export\s+JAVA_HOME="(.*)"')
+    for line in output.splitlines():
+        match = pattern.match(line)
+        if match:
+            return match.group(1)
+    raise ValueError("Cannot find JAVA_HOME")
+
+
+java8_home = Path(
+    detect_javahome(get_cmd_output(["cs", "java", "--jvm", "8", "--env"]))
+)
+java18_home = Path(
+    detect_javahome(get_cmd_output(["cs", "java", "--jvm", "18", "--env"]))
+)
+
+assert java8_home.exists()
+assert java18_home.exists()
+
+
+def get_launcher_path() -> Path:
+    root = Path(
+        "/root/eclipse.jdt.ls/org.eclipse.jdt.ls.product/target/repository/plugins/"
+    )
+    return next(root.glob("org.eclipse.equinox.launcher_*.jar"))
+
+
+config_path = Path(
+    "/root/eclipse.jdt.ls/org.eclipse.jdt.ls.product/target/repository/config_linux"
+)
+
+meta_config = {
+    "d4j_home": "/root/defects4j",
+    "d4j_checkout_root": "/root/d4j-checkout",
+    "jdt_ls_repo": "/root/eclipse.jdt.ls",
+    "java8_home": str(java8_home.absolute()),
+    "language_server_cmd": [
+        str((java18_home / "bin" / "java").absolute()),
+        "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+        "-Dosgi.bundles.defaultStartLevel=4",
+        "-Declipse.product=org.eclipse.jdt.ls.core.product",
+        "-Dlog.level=ERROR",
+        "-noverify",
+        "-Xmx1G",
+        "--add-modules=ALL-SYSTEM",
+        "--add-opens",
+        "java.base/java.util=ALL-UNNAMED",
+        "--add-opens",
+        "java.base/java.lang=ALL-UNNAMED",
+        "-jar",
+        str(get_launcher_path().absolute()),
+        "-configuration",
+        str(config_path.absolute()),
+    ],
+    "seed": 0,
+}
+
+Path("/root/Repilot/meta_config.json").write_text(json.dumps(meta_config))
